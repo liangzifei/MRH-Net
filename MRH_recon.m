fab8e38130f282f971a0f351a483f61c832f9d1b
liangzifei <zifeiliang@hotmail.com>
2021-05-14 16:19:07 -0400

    Add files via upload

--- MRH_recon.m
+++ MRH_recon.m
@@ -1,39 +1,33 @@
-% Using code please refer our work: 
-% Inferring Maps of Cellular Structures from MRI Signals using Deep Learning 
+% Reconstuct slice/volume data from voxel histology generated from MRH_testing.mlx.
+%After running the demo, the result vitual histology will show up.
+
+% -   - Zifei Liang (zifei.liang@nyumc.org)
+
+% Using code please refer our work:
+% Inferring Maps of Cellular Structures from MRI Signals using Deep Learning
 % https://www.biorxiv.org/content/10.1101/2020.05.01.072561v1
+clc; clear; close all;
+%load the predicted voxel-wise histology, one example save under /Output
+load YPred_auto.mat;
 halfsize_input = 1;
 stride = 1;
 count=0;
-loop
-[A,B,C,length]=size(data);
-%     hei = 280; wid = 128;
+[A,B,C,length]=size(YPred);
+% The following hei and wid need to be modified by USERS according to their own data.
+% In our work, the data matrix size is 200x128x280. We process slice by
+% slice. Then one single slice hei = 200, wid = 128; 
 hei = 200; wid = 128;
-ns_data=zeros(hei,wid);fa_data = ns_data; md_data=ns_data; %ref_data = ns_data;
+ns_data=zeros(hei,wid);fa_data = ns_data; %md_data=ns_data; %ref_data = ns_data;
 %     label_data=ns_data;
 for x = 1+halfsize_input : stride : hei-halfsize_input
     for y = 1+halfsize_input :stride : wid-halfsize_input
         count=count+1;
         if count< length
             ns_data(x,y)=YPred(halfsize_input+1,halfsize_input+1,:,count);
+            fa_data(x,y) = fa(halfsize_input+1,halfsize_input+1,:,count);
         else
         end
     end
 end
-ns_data=uint8(ns_data*255);
-figure; imshow(ns_data,[]);
-%     fa_img=load_untouch_nii(['R:\zhangj18lab\zhangj18labspace\Zifei_Data\HCP\',...
-%         'DeepNetIdea\JesseGray\C4\Fitted\fitted_rigidaffine.img']);
-fa_img=load_untouch_nii(['R:\zhangj18lab\zhangj18labspace\Zifei_Data\HCP\DeepNetIdea\JesseGray\JesseGray20191223\Porcessed\test\C1\rigidaffine_Lddm_fa.img']);
-
-fa_data=fa_img.img(:,:,:); fa_data(isnan(fa_data))=0;
-fa_data=permute(fa_data,[1,3,2]); fa_slice = fa_data(:,:,120);
-figure; imshow(fa_slice,[]);
-I = ns_data;
-I=double(I)./255; I=imadjust(I,[min(I(:)) max(I(:))],[]);
-imwrite(uint8(I.*255.*double(logical(fa_slice))),'K:\SRCNN_deltADC\paper_fig\mat_file\fluorescence\test.jpg');
-imwrite(uint8(fa_slice.*256),'K:\SRCNN_deltADC\paper_fig\mat_file\fluorescence\testfa.jpg');
-fa_img=load_untouch_nii(['R:\zhangj18lab\zhangj18labspace\Zifei_Data\',...
-    'HCP\DeepNetIdea\Allen_fluorescence\AllenPathology2TanzilP60.img']);
-fa_data=fa_img.img(:,:,:); fa_data(isnan(fa_data))=0;
-fa_data=permute(fa_data,[1,3,2]); fa_slice = fa_data(:,:,114);
-imwrite(uint8(fa_slice),'K:\SRCNN_deltADC\paper_fig\mat_file\fluorescence\testfa.jpg');
+ns_data = ns_data.*double(logical(fa_data));
+figure; subplot(1,2,1);imshow(ns_data,[]);subplot(1,2,2); imshow(fa_data,[]);
--- demo_testing.m
+++ demo_testing.m
@@ -3,14 +3,20 @@
 % your current folder.
 %  - Zifei Liang (zifei.liang@nyumc.org)
 clc;clear; close all;
-load_data = 'testdataPatch_mouse.mat';
-load_net = 'net_30layerV3Res_HRJG_allMRIs_onesample2fluo5000d.mat';
+% Please relocate your test data and pretrained network locations.
+% load_data under \Test_Data
+%load_net under  \network
+load_data = 'F:\Code\SRCNN\Submit_elife\Pre_trained_Network\testdataPatch_mouse.mat';
+load_net = 'F:\Code\SRCNN\Submit_elife\Pre_trained_Network\net_30layerV3Res_HRJG_allMRIs_Fluo.mat';
 % load_mat =['F:\Code\SRCNN\Fluorescence\traindata.mat'];
 % input_channel = 67;
 % 30 is used in the paper for auto-fluorescence training task, 
 %as large amount data accessable from allen.
 % Shorter is more resaonable under the condition limited training data offered.
 %3 is tested and verified on MR to myelin network training
-depth = 30;
+% depth = 30;
+
 YPred =  MRH_testing(load_data, load_net);
-save YPred.mat YPred;
+% load FA as a reference
+load(load_data,'fa');
+save YPred.mat YPred fa;
