e3088784b1827e7aa0a006ef95561171762bbdc2
liangzifei <zifeiliang@hotmail.com>
2021-05-14 11:42:56 -0400

    Add files via upload

--- demo_training.m
+++ demo_training.m
@@ -1,13 +1,18 @@
-% after running the demo, the data and label file
-% in .mat form like traindataJG_allMRIs_fluo.mat data label -v7.3; should be saved in
-% your current folder.
+% Training the neural network by calling the function MRH_training.
+% After training the network saved in current workspace.
 %  - Zifei Liang (zifei.liang@nyumc.org)
+% Using code please refer our work: 
+% Inferring Maps of Cellular Structures from MRI Signals using Deep Learning 
+% https://www.biorxiv.org/content/10.1101/2020.05.01.072561v1
 clc;clear; close all;
-load_mat =['F:\Code\SRCNN\Fluorescence\traindata.mat'];
+% please replace the location by your saved folder last step.
+load_mat =['F:\Code\SRCNN\Submit_elife\traindata.mat'];
+% please refine the channel according to USER selected MRI contrasts
+% numbers from last step.
 input_channel = 67;
-% 30 is used in the paper for auto-fluorescence training task, 
+%depth: 30 is used in the paper for auto-fluorescence training task, 
 %as large amount data accessable from allen.
 % Shorter is more resaonable under the condition limited training data offered.
 %3 is tested and verified on MR to myelin network training
-depth = 30;
+depth = 15;
 net = MRH_training(load_mat, input_channel, depth);
