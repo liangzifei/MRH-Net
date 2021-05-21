02931721413e011d0c5f52f71b36536e721753f7
liangzifei <zifeiliang@hotmail.com>
2021-05-21 16:35:45 -0400

    Add files via upload

---
+++ Code/Transfer_learning/demo_trainingG_Trans.m
@@ -0,0 +1,20 @@
+% Training data preparison by calling MRH_trainingG_Trans.
+%After running the demo, the data and label file
+% in .mat form like traindataJG_allMRIs_fluo.mat data label -v7.3; should be saved in
+% your current folder.
+
+%  - Zifei Liang (zifei.liang@nyumc.org)
+% Using code please refer our work: 
+% Inferring Maps of Cellular Structures from MRI Signals using Deep Learning 
+% https://www.biorxiv.org/content/10.1101/2020.05.01.072561v1
+
+clc;clear; close all;
+% Please replace by the training data location by USER
+work_folder = ['R:\zhangj18lab\zhangj18labspace\',...
+    'Zifei_Data\HCP\DeepNetIdea\JesseGray\JesseGray20191223\Porcessed\train\'];
+halfpatch_size = 1;
+stride0 = 20;
+% location of the target histology .img. Please replace the location by
+% USER. data location under /Train_data
+myelin_img = load_untouch_nii(['K:\SRCNN_deltADC\Reg_landmark\myelin\Myelin42_to_P60.img']);
+[data,label] = MRH_trainingG_Trans(work_folder,halfpatch_size,stride0, myelin_img);
