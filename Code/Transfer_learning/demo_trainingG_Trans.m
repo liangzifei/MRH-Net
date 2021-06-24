9105c4dfa2244b9f6b8206ec0e5bb8a6e218740e
liangzifei <zifeiliang@hotmail.com>
2021-06-24 16:11:42 -0400

    Add files via upload

--- Code/Transfer_learning/demo_trainingG_Trans.m
+++ Code/Transfer_learning/demo_trainingG_Trans.m
@@ -10,11 +10,11 @@
 
 clc;clear; close all;
 % Please replace by the training data location by USER
-work_folder = ['R:\zhangj18lab\zhangj18labspace\',...
-    'Zifei_Data\HCP\DeepNetIdea\JesseGray\JesseGray20191223\Porcessed\train\'];
+work_folder = ['.\Train_Data\Subj\'];
 halfpatch_size = 1;
 stride0 = 20;
 % location of the target histology .img. Please replace the location by
-% USER. data location under /Train_data
-myelin_img = load_untouch_nii(['K:\SRCNN_deltADC\Reg_landmark\myelin\Myelin42_to_P60.img']);
+% under \Train_Data. Here is the example of MBP 
+% Any other histology please refer under \Train_Data
+myelin_img = load_untouch_nii(['Train_Data\MRI_myelin\Myelin42_to_P60.img']);
 [data,label] = MRH_trainingG_Trans(work_folder,halfpatch_size,stride0, myelin_img);
